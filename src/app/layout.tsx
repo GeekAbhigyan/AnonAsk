cd79d67081b726075f59a2d70a65dd4367ad3d6d
Abhigyan <singhdefinite12@gmail.com>
2024-08-23 01:54:18 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { Toaster } from '@/components/ui/toaster';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'True Feedback',
-  description: 'Real feedback from real people.',
+  title: 'AnonAsk',
+  description: 'Mind murmurs in the classroom..',
 };
 
 interface RootLayoutProps {
